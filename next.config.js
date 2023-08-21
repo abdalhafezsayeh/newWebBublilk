057d2a4a60130c7c7072b79962e31d0fd185e8a4
AbdalhafezSayeh <44759758+abdalhafezsayeh@users.noreply.github.com>
2023-08-21 21:21:06 +0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
+
+module.exports = withPWA({
+  // next.js config
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+});
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    './node_modules/flowbite-react/**/*.js'
+  ],
+  theme: {
+    extend: {
+      colors:{
+          main:{
+            DEFAULT: "#00BFB3",
+            light:"#00BFB31A"
+          },
+          secondary:{
+            DEFAULT:'#E03469',
+          },
+          text:{
+            DEFAULT:'#1E1E1E',
+            light:'#535353',
+          },
+          backGroundNav:{
+            DEFAULT:'rgb(0 0 0 / 86%)'
+          },
+          mobileMain:{
+            DEFAULT: "#02bf02"
+          },
+      },
+      screens:{
+        lg: '940px'
+      },
+      plugins: [require('flowbite/plugin')],
+    },
+  }
+}
